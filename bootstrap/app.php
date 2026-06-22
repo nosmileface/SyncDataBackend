f1433bdf54ef6a6b4cfd1c56ecb61c7262802638
Андрей Евдокимов <aevsergeevich@MacBook-Air-aevsergeevich.local>
2026-06-22 19:52:39 +0400

    fix: Изменено время выполнения команд на "twiceDaily".

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,9 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
 
+const FIRST_SCHEDULE_RUN = 6;
+const SECOND_SCHEDULE_RUN = 18;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -13,13 +16,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withSchedule(function (Schedule $schedule): void {
-        $schedule->command('app:sync-incomes')->everyMinute()->withoutOverlapping();
+        $schedule->command('app:sync-incomes')
+            ->twiceDaily(FIRST_SCHEDULE_RUN, SECOND_SCHEDULE_RUN)
+            ->withoutOverlapping();
 
-        $schedule->command('app:sync-orders')->everyMinute()->withoutOverlapping();
+        $schedule->command('app:sync-orders')
+            ->twiceDaily(FIRST_SCHEDULE_RUN, SECOND_SCHEDULE_RUN)
+            ->withoutOverlapping();
 
-        $schedule->command('app:sync-sales')->everyMinute()->withoutOverlapping();
+        $schedule->command('app:sync-sales')
+            ->twiceDaily(FIRST_SCHEDULE_RUN, SECOND_SCHEDULE_RUN)
+            ->withoutOverlapping();
 
-        $schedule->command('app:sync-stocks')->everyMinute()->withoutOverlapping();
+        $schedule->command('app:sync-stocks')
+            ->twiceDaily(FIRST_SCHEDULE_RUN, SECOND_SCHEDULE_RUN)
+            ->withoutOverlapping();
     })
     ->withMiddleware(function (Middleware $middleware): void {
         //
